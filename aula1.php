4ee6a32994dc9bd8a3e1f96a272ae8a93bdcc925
lndsilva <lndsilva@hotmail.com>
2022-08-08 20:18:59 -0300

    segundo teste em php

--- aula1.php
+++ aula1.php
@@ -1,5 +1,20 @@
-<?php
+<?php 
+	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<?php
 
 	echo "Olá mundo!!!";
+	?>
 
-?>
+</body>
+</html>
+
