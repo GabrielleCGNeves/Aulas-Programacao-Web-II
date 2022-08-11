d42233a53ed9d44bcc0e1fb7627e7ac8ea87a536
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2022-08-11 18:57:13 -0300

    AulasPHP

---
+++ aula02.php
@@ -0,0 +1,24 @@
+<?php
+
+    $valor = 12;
+    $valor = 'Etecia';
+    $valor = true;
+    $valor = 25.5;
+    $valor = "Olá mundo";
+
+    echo 'XD <br>';
+    
+    print ('XD, mas com print <br>');
+
+    print date ('Y <br>');
+    print date ('Y - m - d');
+
+?>
+
+
+<?php 
+
+    $a = 10;
+    echo $a;
+
+?>
