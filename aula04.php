8c6e83dacda8edd4e4e2db40bc1f25ebf7d5c1f1
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2022-08-17 20:18:08 -0300

    Aulas php

---
+++ aula04.php
@@ -0,0 +1,24 @@
+<?php
+print "usando print para exibir na tela" </br>; //o print mostra o que está errado
+
+    $data = "20 de agosto de 2022." //string
+    $salario = 850.00;
+    $cargo = "Estagiario";
+    $idade = 18;
+    $resultado = true; //boolean
+
+    if ($resultado) {
+        echo "Verdadeiro"; //o echo só ecoa o que está escrito
+    }
+
+    $data = "20 de agosto de 2022." //string
+    $salario = 850.00;
+    $cargo = "Estagiario";
+     
+    echo "Arquivo criado em $bata </br>"
+
+    printf ("Salário mínimo: R$ %.2f </br>" $salario);
+    printf ("O salário de um %s é R$ %.2f por mês.", $ )
+
+
+?>
