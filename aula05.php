fbc7fa5a49c4cac0eb74bff3fe68f6e3f48e149b
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2022-08-22 19:51:56 -0300

    Introdução à Função

---
+++ aula05.php
@@ -0,0 +1,13 @@
+<?php
+
+    $ano = 2020; //variavel local no script
+
+    //implementação de uma função
+    function exibir($parametro){
+        $parametro = $parametro + 4;
+        return $parametro; 
+    }
+
+    echo "Estamos em " .$ano. " e daqui a 4 anos."
+
+?>
