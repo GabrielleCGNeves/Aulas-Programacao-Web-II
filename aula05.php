84aac0e35ebfc58ec790cfd1a256efbc04718e43
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2022-08-22 20:01:42 -0300

    Introdução à Função

--- aula05.php
+++ aula05.php
@@ -1,5 +1,22 @@
 <?php
 
+    $nome = "Armando"; //variavel local no script
+
+    //implementação de uma função
+    function exibir(){
+        nome = "Josefina";
+
+        echo "Valor da variável dentro da função " .$nome;
+    }
+
+    exibir(); //chamada de função
+    
+    echo "<br/> Valor da variável fora da função " .$nome;
+
+?>
+
+<?php
+
     $ano = 2020; //variavel local no script
 
     //implementação de uma função
