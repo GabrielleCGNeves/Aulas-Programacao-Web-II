b2db8894ff267b6a3be692b82dc231d28217a6e3
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2022-08-22 20:24:24 -0300

    Introdução à Função

---
+++ aula05_calcular.php
@@ -0,0 +1,14 @@
+<?php
+
+$valor1=10; 
+$valor2=30;
+
+    function soma($valor1 , $valor2){
+
+        return ($valor1 + $valor2);
+        
+    }
+
+     echo "A soma é " .soma($valor1, $valor2);
+
+?>
