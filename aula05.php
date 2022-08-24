31c0fb45d23180ed354788cba72f0a75285954d7
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2022-08-24 20:14:55 -0300

    Funções

--- aula05.php
+++ aula05.php
@@ -1,17 +1,17 @@
 <?php
 
-    $nome = "Armando"; //variavel local no script
+    $nome = "Armando </br>"; //variavel local no script
 
     //implementação de uma função
     function exibir(){
-        nome = "Josefina";
+        $nome = "Josefina </br>";
 
-        echo "Valor da variável dentro da função " .$nome;
+        echo "Valor da variável dentro da função </br>" .$nome;
     }
 
     exibir(); //chamada de função
     
-    echo "<br/> Valor da variável fora da função " .$nome;
+    echo "<br/> Valor da variável fora da função </br>" .$nome;
 
 ?>
 
@@ -20,11 +20,13 @@
     $ano = 2020; //variavel local no script
 
     //implementação de uma função
-    function exibir($parametro){
+    function exibirTexto($parametro){
         $parametro = $parametro + 4;
         return $parametro; 
     }
 
-    echo "Estamos em " .$ano. " e daqui a 4 anos."
+    echo "Estamos em " .$ano. " e daqui a 4 anos. </br>" .exibir($ano);
 
 ?>
+
+
---
+++ aula06.php
@@ -0,0 +1,58 @@
+<?php
+//função sem parametro - se executa por si mesma
+/*
+$ano + 2; 
+$ano = $ano + 2;
+*/ 
+
+    echo "</br> Olá! Seja bem-vindo(a)" .$_SERVER['REMOTE_ADDR'];
+    echo"</br> Você está utilizando o navegador: " .$_SERVER['HTTP_USER_AGENT']."</br>"; 
+
+?>
+
+    <?php
+
+        $ano = 2020; //variavel local no script
+
+        //implementação de uma função
+        function exibirAno(){
+            GLOBAL $ano;
+            $ano++;
+            return $ano; 
+        }
+
+        echo "</br> Ano: " .$ano;
+        echo "</br> Ano: " .exibirAno();
+        echo "</br> Ano: " .exibirAno();
+        echo "</br> Ano: " .$ano;
+    ?>
+
+    <?php
+
+    $ano = 2020; //variavel local no script
+
+    //implementação de uma função
+    function exibirAnoStatic(){
+        STATIC $ano;
+        $ano++;
+        echo "</br>" .$ano; 
+    }
+
+    echo "</br> Ano: " .exibirAnoStatic();
+    echo "</br> Ano: </br>" .exibirAnoStatic();
+
+
+    ?>
+
+    <?php
+        define("PI", 3.14);
+        define("DISC", "Matematica");
+
+        echo "</br> Valor de PI: " .PI;
+        echo "</br> Disciplina: " .DISC;
+    ?>
+
+    <?php
+    //4/2 
+    //4%2 modulo
+    ?>
