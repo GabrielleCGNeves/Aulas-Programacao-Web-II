1c02e55856887a039a050b7e1832454347ffd980
GabriellCGNeves <gabriellecarvalhoneves@gmail.com>
2022-08-29 20:15:23 -0300

    Usando Funções

--- aula07.php
+++ aula07.php
@@ -1,9 +1,38 @@
 <?php
-    $operacao = 1;
+    $operador = 1;
     $num1 = 10;
     $num2 = 2;
     $result = 0.0;
     $operacao = "";
+
+    switch($operador){
+        case 1: 
+            $result = somaValor($num1, $num2); 
+            $operacao = "soma";
+        break;
+
+        case 2: 
+            $result = subValor($num1, $num2); 
+            $operacao = "sub";
+        break;
+
+        case 3: 
+            $result = multValor($num1, $num2); 
+            $operacao = "multiplicação";
+        break;
+
+        case 4: 
+            $result = divValor($num1, $num2); 
+            $operacao = "divisão";
+        break;
+
+        default:
+            echo "Saindo";
+        break;
+    }
+
+    echo "O resultado da " .$operacao. " é de " .$result;
+
 
     function somaValor($num1, $num2){
         return ($num1 + $num2);
@@ -20,33 +49,4 @@
     function divValor($num1, $num2){
         return ($num1 / $num2);
     }
-
-    switch($operacao){
-        case 1: 
-            $result = somaValor(num1, num2); 
-            $operacao = "soma";
-        break;
-
-        case 2: 
-            $result = subValor(num1, num2); 
-            $operacao = "sub";
-        break;
-
-        case 3: 
-            $result = multValor(num1, num2); 
-            $operacao = "multiplicação";
-        break;
-
-        case 4: 
-            $result = divValor(num1, num2); 
-            $operacao = "divisão";
-        break;
-
-       
-        default:
-            echo "Saindo";
-        break;
-    }
-
-    echo "O resultado da " .$operacao. " é de " .$result;
 ?>
