ef9e71ad5b76e978b1c58bc3ffc7153965b445c1
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2022-08-31 20:12:01 -0300

    Expressões

--- aula08.php
+++ aula08.php
@@ -1,9 +1,34 @@
 <?php
 //foraech
-    $nomes = array('Ana', 'Maria', 'Lucas', 'Luiz');
+    $nomes = array('Ana', 'Luiza', 'Lucas', 'Luiz');
 
     foreach ($nomes as $item){
         echo "</br>".$item;
     }
 
+?>
+
+<?php
+
+    for($x = 1; $x < 100; $x++){
+        if($x == 10)
+            goto desvio;
+        echo "</br>".$x. "</br>";
+    }
+
+    desvio:
+    echo"</br> Pronto aqui está o desvio</br>";
+
+?>
+
+<?php
+
+    for($x = 1; $x < 100; $x++){
+        if($x % 2 == 0){
+            continue;
+        }
+        echo "</br>".$x. "</br>";
+    }
+
+//API - PROGRAMA SEM INTERFACE, PROGRAMA FECHADO
 ?>
