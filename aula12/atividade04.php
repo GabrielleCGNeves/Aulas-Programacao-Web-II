d68ea513ff9739fc21b3fad2f15802b40a929d31
Gabrielle Carvalho <88461057+GabrielleCGNeves@users.noreply.github.com>
2022-09-20 14:37:00 -0300

    Complemento da atividade feita em aula
    - Adicionado vetor para armazenar os números
    - Uso da função rsort() para ordenar
    - Uso do algoritmo de ordenamento bubblesort (equivale ao rsort())

--- aula12/atividade04.php
+++ aula12/atividade04.php
@@ -3,32 +3,57 @@
 decrescente (insira números diferentes).
  */
 
-    $n1 = 120;
-    $n2 = 100;
-    $n3 = 90;
+  $num = array(5, 90, 50);
 
-    if($n1 > $n2 && $n2 > $n3){
-        echo $n1. ", " .$n2. " e " .$n3;
-    }
+  //Função própria do PHP para ordernar
+  rsort($num);
 
-    else if($n1 > $n2 && $n3 > $n2){
-        echo $n2. ", " .$n3. " e " .$n1;
-    }
+  echo $num[0]. ", " .$num[1]. " e " .$num[2]. "</br>";
 
-    else if($n2 > $n3 && $n3 > $n1){
-        echo $n2. ", " .$n3. " e " .$n1;
+  //bubble sort PS: COLOCAR EM UMA FUNÇÃO
+  for($i = 0; $i <= sizeof($num); $i++){ //percorre o vetor
+    for($j = 0; $j < sizeof($num); $j++){ // percorre o vetor e ordena os numeros
+      if($num[$j] < ($num[$j+ 1])){
+        $aux = $num[$j];
+        $num[$j] = $num[$j+1];
+        $num[$j+1] = $aux;        
+      }
     }
+  }
+  echo $num[0]. ", " .$num[1]. " e " .$num[2];
 
-    else if($n2 > $n1 && $n1 > $n3){
-        echo $n2. ", " .$n1. " e " .$n3;
-    }
 
-    else if($n3 > $n2 && $n2 > $n1){
-        echo $n3. ", " .$n2. " e " .$n1;
-    }
+  //for each
+  
 
-    else{
-        echo $n3. ", " .$n1. " e " .$n2;
-    }
+
+  // Jeito que fiz na aula
+  $n1 = 90;
+  $n2 = 10;
+  $n3 = 60;
+
+  if($n1 > $n2 && $n2 > $n3){
+      echo $n1. ", " .$n2. " e " .$n3;
+  }
+
+  else if($n1 > $n2 && $n3 > $n2){
+      echo $n2. ", " .$n3. " e " .$n1;
+  }
+
+  else if($n2 > $n3 && $n3 > $n1){
+      echo $n2. ", " .$n3. " e " .$n1;
+  }
+
+  else if($n2 > $n1 && $n1 > $n3){
+      echo $n2. ", " .$n1. " e " .$n3;
+  }
+
+  else if($n3 > $n2 && $n2 > $n1){
+      echo $n3. ", " .$n2. " e " .$n1;
+  }
+
+  else{
+      echo $n3. ", " .$n1. " e " .$n2;
+  }
 
 ?>
