32a9523e673daab5a04352dce1cbea4735b6834c
Gabrielle Carvalho <88461057+GabrielleCGNeves@users.noreply.github.com>
2022-09-25 15:57:38 -0300

    Update atividade03.php

--- aula12/atividade03.php
+++ aula12/atividade03.php
@@ -4,6 +4,26 @@ a. Se a pessoa for do sexo feminino e tiver menos que 25 anos, imprimir nome e a
 mensagem: ACEITA. 
 b. Caso contrário, imprimir nome e a mensagem: NÃO ACEITA. */
 
+  $qtdPessoas = 3;
+  $nome = array("Ana", "Isaac", "Carolina");
+  $sexo = array(1, 2, 1);
+  $idade = array(17, 20, 46);
+
+  for($i = 0; $i < $qtdPessoas; $i++){
+    if($sexo[$i] == 1 && $idade[$i] < 25){
+        echo "Parabéns " .$nome[$i]. "! você foi aceita </br>";
+    }
+
+    else if($sexo[$i] == 1 && $idade[$i] > 25){
+        echo $nome[$i]. ", invelizmente você não foi aceita pois possui mais de 25 anos </br>";
+    }
+      
+    else{
+      echo $nome[$i]. ", infelizmente você não atendeu aos requisitos. </br>";
+    }
+  }
+
+/*
     $nome1 = "Isaac";
     $idade1 = 17;
     $sexo1 = 1;
@@ -23,5 +43,5 @@ b. Caso contrário, imprimir nome e a mensagem: NÃO ACEITA. */
     else{
         echo $nome3. ", infelizmente você não atendeu aos requisitos.";
     }
-
+*/
 ?>
